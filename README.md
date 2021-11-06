6748d45e71d3bd2b87724d855a806f08ee8a898a
Terrance Pitre <tpitre@southleft.com>
2021-11-06 11:20:50 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+## VoxSmart Component Library
+
+This repo is for the VoxSmart component library
+
+### Requirements
+
+TBD.
+
+### Environment setup steps
+
+TBD.
---
+++ voxsmart-component-ui/fractal.config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'VoxSmart Component Library');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'components'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+fractal.web.set('server.port', 4444);
+fractal.web.set('server.watch', true);
+
+/*
+ * Require the Twig adapter
+ */
+const twigAdapter = require('@frctl/twig')();
+fractal.components.engine(twigAdapter);
+fractal.components.set('ext', '.twig');
